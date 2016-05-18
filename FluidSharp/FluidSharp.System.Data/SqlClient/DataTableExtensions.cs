7c8f24b18636ef52b575eb7f4c2aea3f39156e54
Robert Mattingly <rbmattingly@gmail.com>
2016-05-18 16:37:02 -0400

    Added the first DataTable method.

---
+++ FluidSharp/FluidSharp.System.Data/SqlClient/DataTableExtensions.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FluidSharp.System.Data.SqlClient
+{
+    public static class DataTableExtensions
+    {
+        /// <summary>
+        /// Adds the provided DataColumn to the DataTable's DataColumnCollection, returning the DataTable.
+        /// </summary>
+        /// <param name="table">The DataTable to receive the column.</param>
+        /// <param name="column">The DataColumn to add to the DataTable.</param>
+        /// <returns>System.Data.DataTable containing the provided DataColumn.</returns>
+        public static DataTable WithColumn(this DataTable table, DataColumn column)
+        {
+            table.Columns.Add(column);
+            return table;
+        }
+    }
+}
---
+++ FluidSharp/FluidSharp.System.DataTests/SqlClient/DataTableExtensionsTests.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using FluidSharp.System.Data.SqlClient;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+
+namespace FluidSharp.System.Data.SqlClient.Tests
+{
+    [TestClass()]
+    public class DataTableExtensionsTests
+    {
+        [TestMethod()]
+        public void WithColumn_Will_AddTheProvidedColumn_ToTheDataTable()
+        {
+            var columnToAdd = new DataColumn("NewColumn");
+
+            var tableToReceiveColumn = new DataTable();
+
+            var tableWithColumn = tableToReceiveColumn.WithColumn(columnToAdd);
+
+            Assert.ReferenceEquals(columnToAdd, tableWithColumn.Columns[0]);
+        }
+    }
+}
