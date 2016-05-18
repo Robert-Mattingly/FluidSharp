169e026b0ef1dc0f78fb8b9ff73f05fca42c1f54
Robert Mattingly <rbmattingly@gmail.com>
2016-05-18 16:24:16 -0400

    Created first SqlParameter extension method.

---
+++ FluidSharp/FluidSharp.System.Data/SqlClient/SqlParameter.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FluidSharp.System.Data.SqlClient
+{
+    /// <summary>
+    /// A collection of methods to ease interactions with SqlParameter objects.
+    /// </summary>
+    public static class SqlParameterExtensions
+    {
+        /// <summary>
+        /// Sets the value of the SqlParameter to the passed value.
+        /// </summary>
+        /// <param name="param">The SqlParameter whose value will be set.</param>
+        /// <param name="value">The value to set.</param>
+        /// <returns>The SqlParameter with the updated value.</returns>
+        public static SqlParameter WithValue(this SqlParameter param, object value)
+        {
+            param.Value = value;
+            return param;
+        }
+    }
+}
---
+++ FluidSharp/FluidSharp.System.DataTests/SqlClient/SqlParameterExtensionsTests.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using FluidSharp.System.Data.SqlClient;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+
+namespace FluidSharp.System.Data.SqlClient.Tests
+{
+    [TestClass()]
+    public class SqlParameterExtensionsTests
+    {
+        [TestMethod()]
+        public void WithValue_Will_ReturnTheSameSqlParameter()
+        {
+            var param = new SqlParameter();
+
+            var paramWithValue = param.WithValue(4);
+
+            Assert.ReferenceEquals(param, paramWithValue);
+        }
+
+        [TestMethod]
+        public void WithValue_Will_SetTheSqlParametersValue_ToThePassedValue()
+        {
+            var param = new SqlParameter().WithValue("TestValue");
+
+            Assert.AreEqual("TestValue", param.Value);
+        }
+    }
+}
