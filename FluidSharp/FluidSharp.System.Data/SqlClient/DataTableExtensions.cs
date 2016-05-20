2ae54272059e841e0c44f4e05c627121de4f0bec
Robert Mattingly <rbmattingly@gmail.com>
2016-05-20 09:04:58 -0400

    Added WithColumn extension method for all forms of System.Data.DataTable.Columns.Add() and an AddNewRow method.

--- FluidSharp/FluidSharp.System.Data/SqlClient/DataTableExtensions.cs
+++ FluidSharp/FluidSharp.System.Data/SqlClient/DataTableExtensions.cs
@@ -10,7 +10,30 @@ namespace FluidSharp.System.Data.SqlClient
     public static class DataTableExtensions
     {
         /// <summary>
-        /// Adds the provided DataColumn to the DataTable's DataColumnCollection, returning the DataTable.
+        /// Creates a new DataRow object matching the DataTable's schema and adds it to the DataRowCollection object.
+        /// </summary>
+        /// <param name="table">The DataTable to receive the created DataRow.</param>
+        /// <returns>System.Data.DataTable containing the created DataRow.</returns>
+        public static DataRow AddNewRow(this DataTable table)
+        {
+            var newRow = table.NewRow();
+            table.Rows.Add(newRow);
+            return newRow;
+        }
+
+        /// <summary>
+        /// Creates and adds a new DataColumn object to the DataTable.
+        /// </summary>
+        /// <param name="table">The DataTable to receive the created DataColumn.</param>
+        /// <returns>System.Data.DataTable containing the created DataColumn.</returns>
+        public static DataTable WithColumn(this DataTable table)
+        {
+            table.Columns.Add();
+            return table;
+        }
+
+        /// <summary>
+        /// Adds the provided DataColumn to the DataTable's DataColumnCollection.
         /// </summary>
         /// <param name="table">The DataTable to receive the column.</param>
         /// <param name="column">The DataColumn to add to the DataTable.</param>
@@ -18,6 +41,51 @@ namespace FluidSharp.System.Data.SqlClient
         public static DataTable WithColumn(this DataTable table, DataColumn column)
         {
             table.Columns.Add(column);
+            return table;
+        }
+
+        /// <summary>
+        /// Creates a new DataColumn and adds it to the DataTable's DataColumnCollection.
+        /// </summary>
+        /// <param name="table">The DataTable to receive the created DataColumn.</param>
+        /// <param name="columnName">The name to assign to the DataColumn's ColumnName property.</param>
+        /// <returns>System.Data.DataTable containing the created DataColumn.</returns>
+        public static DataTable WithColumn(this DataTable table, string columnName)
+        {
+            table.Columns.Add(columnName);
+            return table;
+        }
+        
+
+        /// <summary>
+        /// Creates a new DataColumn and adds it to the DataTable's DataColumnCollection.
+        /// </summary>
+        /// <param name="table">The DataTable to receive the created DataColumn.</param>
+        /// <param name="columnName">The name to assign to the created DataColumn's ColumnName property.</param>
+        /// <param name="type">The Type to assign to the created DataColumn's DataType property.</param>
+        /// <returns>System.Data.DataTable containing the created DataColumn.</returns>
+        public static DataTable WithColumn(this DataTable table, string columnName, Type type)
+        {
+            table.Columns.Add(columnName, type);
+            return table;
+        }
+
+        /// <summary>
+        /// Creates a new DataColumn and adds it to the DataTable's DataColumnCollection.
+        /// </summary>
+        /// <param name="table">The DataTable to receive the created DataColumn.</param>
+        /// <param name="columnName">The name to assign to the created DataColumn's ColumnName property.</param>
+        /// <param name="type">The Type to assign to the created DataColumn's DataType property.</param>
+        /// <param name="expression">The string expression to assign to the created DataColumn's Expression property.  
+        /// (e.g. To create a column which sums the value of the Hourly and OverTime columns, "Hourly + OverTime".)</param>
+        /// <returns>System.Data.DataTable containing the created DataColumn.</returns>
+        /// <remarks>
+        /// For more information on using the Expression property, please review the 
+        /// MSDN documentation at https://msdn.microsoft.com/en-us/library/system.data.datacolumn.expression%28v=vs.110%29.aspx.
+        /// </remarks>
+        public static DataTable WithColumn(this DataTable table, string columnName, Type type, string expression)
+        {
+            table.Columns.Add(columnName, type, expression);
             return table;
         }
     }
--- FluidSharp/FluidSharp.System.DataTests/SqlClient/DataTableExtensionsTests.cs
+++ FluidSharp/FluidSharp.System.DataTests/SqlClient/DataTableExtensionsTests.cs
@@ -12,6 +12,50 @@ namespace FluidSharp.System.Data.SqlClient.Tests
     [TestClass()]
     public class DataTableExtensionsTests
     {
+        [TestMethod]
+        public void AddNewRow_Will_AddARowToTheDataTable()
+        {
+            var table = new DataTable();
+
+            table.AddNewRow();
+
+            Assert.AreEqual(1, table.Rows.Count);
+        }
+
+        [TestMethod]
+        public void AddNewRow_Will_CreateARowWithTheSameSchema_AsTheDataTable()
+        {
+            var table = new DataTable().WithColumn("Name").WithColumn("Age");
+
+            var row = table.AddNewRow();
+
+            Assert.AreEqual(2, row.ItemArray.Length);
+        }
+
+        [TestMethod]
+        public void AddNewRow_Will_ReturnTheCreatedRow()
+        {
+            var table = new DataTable();
+
+            var newRow = table.AddNewRow();
+
+            Assert.ReferenceEquals(table.Rows[0], newRow);
+        }
+
+        [TestMethod]
+        public void WithColumn_Will_AddACreatedColumn_ToTheDataTable()
+            => Assert.AreEqual(1, new DataTable().WithColumn().Columns.Count);
+
+        [TestMethod]
+        public void WithColumn_Will_ReturnTheSameDataTable()
+        {
+            var originalTable = new DataTable();
+
+            var tableWithColumn = originalTable.WithColumn();
+
+            Assert.ReferenceEquals(originalTable, tableWithColumn);
+        }
+
         [TestMethod()]
         public void WithColumn_Will_AddTheProvidedColumn_ToTheDataTable()
         {
@@ -22,6 +66,80 @@ namespace FluidSharp.System.Data.SqlClient.Tests
             var tableWithColumn = tableToReceiveColumn.WithColumn(columnToAdd);
 
             Assert.ReferenceEquals(columnToAdd, tableWithColumn.Columns[0]);
+        }
+
+        [TestMethod]
+        public void WithColumn_Will_ReturnTheSameDataTable_WhenPassedDataColumn()
+        {
+            var columnToAdd = new DataColumn("NewColumn");
+
+            var originalTable = new DataTable();
+
+            var tableWithColumn = originalTable.WithColumn(columnToAdd);
+
+            Assert.ReferenceEquals(originalTable, tableWithColumn);
+        }
+
+        [TestMethod()]
+        public void WithColumn_Will_AddTheCreatedColumn_WithTheProvidedColumnName_ToTheDataTable()
+            => Assert.AreEqual("NewColumn", new DataTable().WithColumn("NewColumn").Columns[0].ColumnName);
+
+        [TestMethod]
+        public void WithColumn_Will_ReturnTheSameDataTable_WhenPassedColumnName()
+        {
+            var originalTable = new DataTable();
+
+            var tableWithColumn = originalTable.WithColumn("NewColumn");
+
+            Assert.ReferenceEquals(originalTable, tableWithColumn);
+        }
+
+        [TestMethod()]
+        public void WithColumn_Will_AddTheCreatedColumn_WithTheProvidedColumnNameAndType_ToTheDataTable()
+        {
+            var expectedValues = new Tuple<string, Type>("NewColumn", typeof(int));
+
+            var table = new DataTable().WithColumn("NewColumn", typeof(int));
+
+            var resultantValues = new Tuple<string, Type>(table.Columns[0].ColumnName, table.Columns[0].DataType);
+
+            Assert.AreEqual(expectedValues, resultantValues);
+        }
+
+        [TestMethod]
+        public void WithColumn_Will_ReturnTheSameDataTable_WhenPassedColumnNameAndType()
+        {
+            var originalTable = new DataTable();
+
+            var tableWithColumn = originalTable.WithColumn("NewColumn", typeof(int));
+
+            Assert.ReferenceEquals(originalTable, tableWithColumn);
+        }
+
+        [TestMethod()]
+        public void WithColumn_Will_AddTheCreatedColumn_WithTheProvidedColumnNameAndTypeAndExpression_ToTheDataTable()
+        {
+            var expectedValues = new Tuple<string, Type, string>("NewColumn", typeof(int), "Hourly + OverTime");
+
+            var table = new DataTable().WithColumn("Hourly")
+                                       .WithColumn("OverTime")
+                                       .WithColumn("NewColumn", typeof(int), "Hourly + OverTime");
+
+            var resultantValues = new Tuple<string, Type, string>(table.Columns[2].ColumnName, table.Columns[2].DataType, table.Columns[2].Expression);
+
+            Assert.AreEqual(expectedValues, resultantValues);
+        }
+
+        [TestMethod]
+        public void WithColumn_Will_ReturnTheSameDataTable_WhenPassedColumnNameAndTypeAndExpression()
+        {
+            var originalTable = new DataTable();
+
+            var tableWithColumn = originalTable.WithColumn("Hourly")
+                                               .WithColumn("OverTime")
+                                               .WithColumn("NewColumn", typeof(int), "Hourly + OverTime");
+
+            Assert.ReferenceEquals(originalTable, tableWithColumn);
         }
     }
 }
